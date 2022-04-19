60584dab45e346e0c55ea44673d74d72c0c74b37
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-04-19 17:55:46 -0300

    nodemodules reset

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 export default class Login extends Component {
   render() {
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        <form>
+          Login
+        </form>
+      </div>
     );
   }
 }
